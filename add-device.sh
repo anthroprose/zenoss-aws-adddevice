f41558332d598e30f579b9070a70f54dc27d700a
Alex Corley <anthroprose@gmail.com>
2013-08-07 00:20:32 -0500

    Updated some bash

--- add-device.sh
+++ add-device.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-$DEVICECLASS = "/Server/SSH/Linux"
-$PROTO = "https"
-$HOST = ""
-$USER = ""
-$PASS = ""
+$DEVICECLASS="/Server/SSH/Linux"
+$PROTO="http"
+$HOST=""
+$USER=""
+$PASS=""
+##############################################################
+
+$MAC=`GET http://169.254.169.254/latest/meta-data/network/interfaces/macs`
+$PRIVATEIP=`GET http://169.254.169.254/latest/meta-data/network/interfaces/macs/${MAC}/local-ipv4s/
 curl --insecure -u "${USER}:${PASS}" -X POST -H "Content-Type: application/json" -d \'{"action":"DeviceRouter","method":"addDevice","data":[{"deviceName":"${HOSTNAME}","deviceClass":"${DEVICECLASS}"}],"tid":1}\' ${PROTO}://${HOST}/zport/dmd/device_router'
